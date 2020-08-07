29f03b4677af1de9b6d4cd98f955e656eaf0aa05
Josh Herrera <joshherrera09@gmail.com>
2020-08-07 11:43:26 -0700

    Temp/humidity added to 5 day forecast

--- script.js
+++ script.js
@@ -11,34 +11,24 @@ function displayCityInfo() {
         url:queryURL,
         method: 'GET'
     }).then(function(response) {
-        // console.log(queryURL);
-        // console.log(response);
         var farTemp = response.main.temp
-        // console.log(farTemp);
         var cityName = response.name;
         var humidity = response.main.humidity;
         var windSpeed = response.wind.speed;
         var iconCode = response.weather[0].icon;
-        var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
-
-        // console.log(cityName);
-        // console.log(humidity);
-        // console.log(windSpeed);
+        var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
 
         $('#city-date').append(cityName + ' ');
         $('#city-date').append(moment().format('[(]M/D/YYYY[)]'));
         $('#wicon').attr('src', iconUrl);
-
         $('#temp').append('Temperature: ' + farTemp);
         $('#humidity').append('Humidity: ' + humidity);
         $('#windspeed').append('Wind Speed: ' + windSpeed + ' MPH');
 
 
-        
-
     });
 }
-
+// Funciton to fill the five day forecast
 function fiveDay () {
     var city = 'san diego';
     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
@@ -50,29 +40,83 @@ function fiveDay () {
         console.log(response);
 
         // First day of forecast
+        var dayOneTemp = response.list[5].main.temp;        
+        var dayOnehumidity = response.list[5].main.humidity;
+        var dayOneDate = moment().add(1,'days').format('DD/MM/YYYY');
         var dayOneTemp = response.list[5].main.temp;
-        console.log(dayOneTemp);
-        var humidity = response.list[5].main.humidity;
-        console.log(humidity);
-        var date = moment().add(1,'days').format('DD/MM/YYYY');
-        console.log(date);
-        var temp = response.list[5].main.temp;
-        console.log(temp);
+        // var iconCode = response.weather[0].icon;
+        // var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
 
-        $('#dayOneDate').append(date);
+        $('#dayOneDate').append(dayOneDate);
+        // $('#dayOneIcon').attr('src', iconUrl);
         $('#dayOneTemp').append('Temp:' + dayOneTemp);
-        $('#dayOneHum').append('Humidity:' + humidity + '%');
+        $('#dayOneHum').append('Humidity:' + dayOnehumidity + '%');
+
+        // Second day of forecast
+        var dayTwoTemp = response.list[12].main.temp;        
+        var dayTwoHumidity = response.list[12].main.humidity;
+        var dayTwoDate = moment().add(2,'days').format('DD/MM/YYYY');
+        var dayTwoTemp = response.list[12].main.temp;
+        // var iconCode = response.weather[0].icon;
+        // var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+
+        $('#dayTwoDate').append(dayTwoDate);
+        // $('#dayOneIcon').attr('src', iconUrl);
+        $('#dayTwoTemp').append('Temp:' + dayTwoTemp);
+        $('#dayTwoHum').append('Humidity:' + dayTwoHumidity + '%');
+
+        // Third day of forecast
+        var dayThreeTemp = response.list[20].main.temp;        
+        var dayThreeHumidity = response.list[20].main.humidity;
+        var dayThreeDate = moment().add(3,'days').format('DD/MM/YYYY');
+        var dayThreeTemp = response.list[20].main.temp;
+        // var iconCode = response.weather[0].icon;
+        // var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+
+        $('#dayThreeDate').append(dayThreeDate);
+        // $('#dayOneIcon').attr('src', iconUrl);
+        $('#dayThreeTemp').append('Temp:' + dayThreeTemp);
+        $('#dayThreeHum').append('Humidity:' + dayThreeHumidity + '%');
+
+        // Fourth day of forecast
+        var dayFourTemp = response.list[28].main.temp;        
+        var dayFourHumidity = response.list[28].main.humidity;
+        var dayFourDate = moment().add(3,'days').format('DD/MM/YYYY');
+        var dayFourTemp = response.list[28].main.temp;
+        // var iconCode = response.weather[0].icon;
+        // var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+
+        $('#dayFourDate').append(dayFourDate);
+        // $('#dayOneIcon').attr('src', iconUrl);
+        $('#dayFourTemp').append('Temp:' + dayFourTemp);
+        $('#dayFourHum').append('Humidity:' + dayFourHumidity + '%');
+
+        // Fifth day of forecast
+        var dayFiveTemp = response.list[36].main.temp;        
+        var dayFiveHumidity = response.list[36].main.humidity;
+        var dayFiveDate = moment().add(3,'days').format('DD/MM/YYYY');
+        var dayFiveTemp = response.list[36].main.temp;
+        // var iconCode = response.weather[0].icon;
+        // var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+
+        $('#dayFiveDate').append(dayFiveDate);
+        // $('#dayOneIcon').attr('src', iconUrl);
+        $('#dayFiveTemp').append('Temp:' + dayFiveTemp);
+        $('#dayFiveHum').append('Humidity:' + dayFiveHumidity + '%');
         
     })
 }
+
+// UV index call
 
 
 displayCityInfo();
 fiveDay();
 
+//TO-DO
 
+// Fix cards(display date, style, display wether icon)
+// Finsish styling
 
 // Array to store recent searches
-
-// 5 day forecast url for San Diego https://api.openweathermap.org/data/2.5/onecall?lat=32.72&lon=-117.16&%20exclude=minutely,hourly&appid=99adabcd9b9526ae2fc8e7bbc24f5de4
-// 5 day forecast url for any city https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
+// Funnction to store recent searches to local storage
