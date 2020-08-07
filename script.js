0a5627725f196e198021116f53202aa9b1c7bf98
Josh Herrera <joshherrera09@gmail.com>
2020-08-07 11:11:17 -0700

    working on forecast cards

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 function displayCityInfo() {
     // var city = $('.city-search').val();
     var city = 'san diego';
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
     
 
     $.ajax({
@@ -13,7 +13,7 @@ function displayCityInfo() {
     }).then(function(response) {
         // console.log(queryURL);
         // console.log(response);
-        var farTemp = (response.main.temp - 273.15) * 1.80 +32;
+        var farTemp = response.main.temp
         // console.log(farTemp);
         var cityName = response.name;
         var humidity = response.main.humidity;
@@ -41,20 +41,34 @@ function displayCityInfo() {
 
 function fiveDay () {
     var city = 'san diego';
-    var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=32.72&lon=-117.16&%20exclude=minutely,hourly&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
+    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
 
     $.ajax({
-        url:queryURL;
-        method:'GET'
+        url:queryURL,
+        method: 'GET'
     }).then(function(response) {
+        console.log(response);
 
-        
+        // First day of forecast
+        var dayOneTemp = response.list[5].main.temp;
+        console.log(dayOneTemp);
+        var humidity = response.list[5].main.humidity;
+        console.log(humidity);
+        var date = moment().add(1,'days').format('DD/MM/YYYY');
+        console.log(date);
+        var temp = response.list[5].main.temp;
+        console.log(temp);
+
+        $('#dayOneDate').append(date);
+        $('#dayOneTemp').append('Temp:' + dayOneTemp);
+        $('#dayOneHum').append('Humidity:' + humidity + '%');
         
     })
 }
 
 
 displayCityInfo();
+fiveDay();
 
 
 
